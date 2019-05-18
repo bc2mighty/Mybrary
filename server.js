3223f240a7e13e8dfdec1e51bc40fa02a844bb66
Brave Crown <akinadetunmise8@gmail.com>
2019-05-18 15:18:55 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const express = require("express");
 const app = express();
 
 const expresslayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine","ejs");
 app.set("views",__dirname + "/views");
 app.set("layout","layouts/layout");
 app.use(expresslayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: "10mb", extended: false}));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -26,5 +29,6 @@ db.on("error", error => console.log(error));
 db.once("open", () => console.log("Connected!"));
 
 app.use("/",indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
